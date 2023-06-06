40ef3cae69df43c212f83c34805724cfe7f39a8d
Yulia Lopatko <engineeryulia@gmail.com>
2023-06-05 18:35:23 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <header>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
