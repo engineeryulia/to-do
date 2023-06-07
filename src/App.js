0f0977208e18ce3f06330ee89801690f23899a1b
Yulia Lopatko <99161649+engineeryulia@users.noreply.github.com>
2023-06-07 13:48:56 -0600

    lesson_1_2 (#2)
    * lesson_1_2
    
    * fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import './App.css';
-const todoList = [
-    {
-        id: 1,
-        title: 'one'
-    },
-    {   id: 2,
-        title: 'two'
-    },
 
-    {   id: 3,
-        title: 'three'
-    }
-    ];
+import React from 'react';
+import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
+
+
 function App() {
   return (
     <div className="App">
       <h1> Todo List:</h1>
-        <ul>
-            {todoList.map(function (item) {
-                return <li key={item.id}>{item.title}</li>;
-            })}
-        </ul>
+
+        <TodoList />
+        <AddTodoForm />
+      
     </div>
   );
 };
+
+export default App;
